5a739ca7a266cdca85d5d1f24e54db278ef4923b
SDen4 <asw325@yandex.ru>
2021-08-19 21:29:49 +0500

    styles:background

---
+++ src/view/Root/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Root.css';
+
+const Root: React.FC = () => {
+  return (
+    <div className="root_wrapper">
+      <header className="root_header">Find github&apos;s user</header>
+    </div>
+  );
+};
+
+export default Root;
