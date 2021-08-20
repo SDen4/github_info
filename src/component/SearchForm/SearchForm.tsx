3735721e5247a0edde0ccc9c2ea4e8e5c99b8aa1
SDen4 <asw325@yandex.ru>
2021-08-20 21:00:24 +0500

    update SearchForm component

--- src/component/SearchForm/SearchForm.tsx
+++ src/component/SearchForm/SearchForm.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 
 const SearchForm: React.FC = () => {
+  const [searchText, setSearchText] = useState<string>('');
+
+  const changeTextHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event?.target.value);
+  };
+
   return (
-    <input className="input" type="text" placeholder="Enter the github login" />
+    <form>
+      <input
+        className="input"
+        type="text"
+        placeholder="Enter the github login"
+        value={searchText}
+        onChange={changeTextHandler}
+      />
+      <button type="submit">Search</button>
+    </form>
   );
 };
 
