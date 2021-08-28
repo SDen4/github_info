74f5453d21f926e32311c5b2e9758f639d6ae802
SDen4 <asw325@yandex.ru>
2021-08-28 18:57:48 +0500

    click on user in follow list logic

--- src/components/UserItem/UserItem.tsx
+++ src/components/UserItem/UserItem.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { searchSaga } from '../../store/SearchReducer/actions';
 
 import { IUserItem } from './types';
 
 import './styles.css';
 
 const UserItem: React.FC<IUserItem> = ({ user }) => {
+  const dispatch = useDispatch();
+
+  const itemBthHandler = () => {
+    dispatch(searchSaga(user.login));
+  };
+
   return (
     <div className="user_item_wrapper">
       <div className="user_item_photo_wrapper">
@@ -13,7 +22,9 @@ const UserItem: React.FC<IUserItem> = ({ user }) => {
 
       <div className="user_info">
         <div className="user_info_unit">
-          <h2>{user.login}</h2>
+          <button type="button" onClick={itemBthHandler}>
+            <h2>{user.login}</h2>
+          </button>
         </div>
       </div>
     </div>
