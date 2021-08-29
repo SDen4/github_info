2406a12e7de8c0605f586bbdc666ad8c1d6f8a5b
SDen4 <asw325@yandex.ru>
2021-08-29 14:16:13 +0500

    Search history list logic

--- src/components/SearchHistoryList/SearchHistoryList.tsx
+++ src/components/SearchHistoryList/SearchHistoryList.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { searchSaga } from '../../store/SearchReducer/actions';
+
+import { dateFormatter } from '../../utils/dateFormatter';
 
 import { ISearchHistoryList } from './types';
 
 import styles from './SearchHistoryList.module.css';
 
 const SearchHistoryList: React.FC<ISearchHistoryList> = ({ searchList }) => {
-  // eslint-disable-next-line no-console
-  console.log(searchList);
+  const dispatch = useDispatch();
+
+  const searchHistoriListBtnHandler = (login: string) => {
+    dispatch(searchSaga(login));
+  };
+
   return (
     <div className={styles.shl_wrapper}>
       <h3>Search history list</h3>
+      <ol>
+        {searchList.map((el) => (
+          <li key={el.login}>
+            <button
+              className={styles.shl_button}
+              type="button"
+              onClick={() => searchHistoriListBtnHandler(el.login)}
+            >
+              {el.login}
+            </button>
+            <span> ({dateFormatter(el.dateOfSearch)})</span>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
