6b8ce90e35679fc40a49862510a6d8a23d0a1f13
SDen4 <asw325@yandex.ru>
2022-10-16 15:03:54 +0500

    refactor: new typing of Flex component

--- src/ui/Flex/Flex.tsx
+++ src/ui/Flex/Flex.tsx
@@ -1,15 +1,8 @@
-import React, { memo } from 'react';
+import React, { HTMLAttributes, memo } from 'react';
 
 import classes from './styles.module.css';
 
-interface IProps {
-  className?: string;
-  props?: any;
-  onClick?: () => void;
-  style?: React.CSSProperties | undefined;
-}
-
-export const Flex: React.FC<IProps> = memo(
+export const Flex: React.FC<HTMLAttributes<HTMLDivElement>> = memo(
   ({ children, className, style, ...props }) => {
     return (
       <div className={`${classes.flex} ${className}`} style={style} {...props}>
