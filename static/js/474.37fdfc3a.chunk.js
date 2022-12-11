219c7ffeac8e452b72ea4781c49401033d0deaf7
SDen4 <asw325@yandex.ru>
2022-12-11 21:55:19 +0500

    Updates

---
+++ static/js/474.37fdfc3a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgithub_info=self.webpackChunkgithub_info||[]).push([[474],{474:function(n,e,r){r.r(e),r.d(e,{default:function(){return i}});r(2791);var s=r(9082),u="styles_errorWrapper__8oNm7",t=r(184),i=function(){return(0,t.jsx)(s.k,{className:u,children:(0,t.jsx)("h2",{children:"User is not found..."})})}}}]);
+//# sourceMappingURL=474.37fdfc3a.chunk.js.map
