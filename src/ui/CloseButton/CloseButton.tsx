6c543838328ddfead2e959c564ddc6c81b549314
SDen4 <asw325@yandex.ru>
2023-01-19 22:28:22 +0500

    refactor: CloseButton

--- src/ui/CloseButton/CloseButton.tsx
+++ src/ui/CloseButton/CloseButton.tsx
@@ -1,19 +1,17 @@
-import React, { memo } from 'react';
+import React from 'react';
 import clsx from 'clsx';
 
 import styles from './styles.module.css';
 
-export const CloseButton: React.FC<{ onClick: () => void }> = memo(
-  ({ onClick }) => {
-    return (
-      <button
-        className={clsx(styles.closeBtn, styles.tooltip)}
-        type="button"
-        aria-label="Close"
-        onClick={onClick}
-      >
-        <div className={styles.tooltipText}>Close</div>
-      </button>
-    );
-  },
-);
+export const CloseButton: React.FC<{ onClick: () => void }> = ({ onClick }) => {
+  return (
+    <button
+      className={clsx(styles.closeBtn, styles.tooltip)}
+      type="button"
+      aria-label="Close"
+      onClick={onClick}
+    >
+      <div className={styles.tooltipText}>Close</div>
+    </button>
+  );
+};
